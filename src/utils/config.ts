13ce46af9ce2b419417f4389b00ca219e3f935a0
Herbert Joseph <techniquea2z@gmail.com>
2025-05-19 11:55:27 -0400

    fix(netlify): testing netlify deploymentfrom astro

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -20,7 +20,7 @@ export interface MetaDataConfig extends Omit<MetaData, 'title'> {
 
 const config = siteConfig as any;
 
-const DEFAULT_SITE_NAME = 'Senpai | Dating App for Anime Lovers';
+const DEFAULT_SITE_NAME = 'Hexborn | Indie Action RPG';
 
 const getSite = () => {
   const _default = {
